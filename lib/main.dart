392a9b2dc6202db5c6e330db9a08c34d8a8be325
Raju Kumar <kraju751@gmail.com>
2023-04-11 23:27:59 +0530

    Adding multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BlocProvider<LoginCubit>(
-        create: (context) => LoginCubit(),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider<LoginCubit>(
+            create: (context) => LoginCubit(),
+          ),
+        ],
         child: const Login(),
       ),
     );
